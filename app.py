d6cafb74e422b2aa9822969b97d47b01242abb42
isawzz <iwgajam@gmail.com>
2022-01-30 20:20:11 -0800

    das

--- app.py
+++ app.py
@@ -32,44 +32,50 @@ def index1():
 def index2():
 	return send_from_directory('front2', 'index.html')
 
+#endregion
+
 #region jinja 2 (flask template) routes: mix python into html
 @app.route('/', methods=['POST', 'GET'])
-def index():
+def template():
 	if request.method == 'POST':
 		#return 'Hello!' #works when click auf 'Add Task' button
+		#add posted data as new task
 		task_content = request.form['content'] # should be id of input (see index.html)
 		new_task = Todo(content=task_content)
-		# task_content = request.form['content']
-		# new_task = Todo(content=task_content)
 		try:
 			db.session.add(new_task)
 			db.session.commit()
 			return redirect('/')
 		except:
-			return 'There was an issue adding your task'
-
+			return 'error adding task!'
 	else:
-		tasks = Todo.query.order_by(Todo.date_created).all()
+		#show tasks from test.db in templates/index.html
+		tasks = Todo.query.order_by(Todo.date_created).all() #.first(), 
 		return render_template('index.html', tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+	task_to_delete = Todo.query.get_or_404(id)
+	try:
+		db.session.delete(task_to_delete)
+		db.session.commit()
+		return redirect('/')
+	except:
+		return 'There was a problem deleting that task'
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+	task = Todo.query.get_or_404(id)
+	if request.method == 'POST':
+		task.content = request.form['content']
+		try:
+			db.session.commit()
+			return redirect('/')
+		except:
+			return 'There was an issue updating your task'
+	else:
+		return render_template('update.html', task=task)
 
-# @app.route('/', methods=['POST', 'GET'])
-# def template():
-# 	if request.method == 'POST':
-# 		#return 'Hello!' #works when click auf 'Add Task' button
-# 		task_content = request.form['content'] # should be id of input (see index.html)
-# 		new_task = Todo(content=task_content)
-# 		try:
-# 			db.session.add(new_task)
-# 			db.session.commit()
-# 			return redirect('/', tasks=tasks)
-# 		except:
-# 			tasks = Todo.query.order_by(Todo.date_created).all() #.first(), 
-# 			return 'error adding task!'
-
-# 	else:
-# 		return render_template('index.html')
 
 
 if __name__ == "__main__":
--- templates/js/start.js
+++ templates/js/start.js
@@ -3,10 +3,11 @@ window.onload = start;
 async function start() {
 
 	let dMain = mBy('dMain');
-	mCenterFlex(dMain);
+	// mStyleX(dMain,{align:'center'});
+	// //mCenterFlex(dMain);
 
-	let dForm = mBy('dForm');
-	mStyleX(dForm,{matop:20})
+	// let dForm = mBy('dForm');
+	// mStyleX(dForm,{matop:20})
 
-	let dStatus = mDiv(dMain,{matop:25,fg:'red'},'dStatus','wirklich? so einfach geht das???');
+	let dStatus = mDiv(dMain,{align:'center',matop:25,fg:'red'},'dStatus','wirklich? so einfach geht das???');
 }
