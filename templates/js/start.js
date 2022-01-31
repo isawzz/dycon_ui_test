579c644afece73bfe8e38ee2837031cb77cae911
isawzz <iwgajam@gmail.com>
2022-01-30 20:36:21 -0800

    das

--- templates/js/start.js
+++ templates/js/start.js
@@ -3,11 +3,5 @@ window.onload = start;
 async function start() {
 
 	let dMain = mBy('dMain');
-	// mStyleX(dMain,{align:'center'});
-	// //mCenterFlex(dMain);
-
-	// let dForm = mBy('dForm');
-	// mStyleX(dForm,{matop:20})
-
 	let dStatus = mDiv(dMain,{align:'center',matop:25,fg:'red'},'dStatus','wirklich? so einfach geht das???');
 }
